ccb0b69091c4cce549bd8ba9c377aafa91149593
Felipe Mateus <felipe_mateus08@hotmail.com>
2022-12-30 16:33:55 -0300

    docs: desafio concluido

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,78 @@ import { useState } from 'react';
 // * Não é permitido usar refs
 //
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
-// todo - Desabilite o botão de Login equanto você está executando o login.
-// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+// ✅ - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// ✅ - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos. 
+// ✅ - Desabilite o botão de Login equanto você está executando o login.
+// ✅ - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// ✅ - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [auth, setAuth] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = auth;
+  const disabled = email.length === 0 || password.length < 6 || loading;
+  
+  const onChangeInput = (event) => {
+    const { value } = event.target;
+
+    setAuth({
+      ...auth,
+      [event.target.id]: value,
+    });
+  };
+
+  const onSubmitLogin = (event) => {
+    event.preventDefault();
+
+    setError(null);
+    setLoading(true);
+
+    login({ email, password })
+      .then(() => {
+        alert('Login efetuado com sucesso!');
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className='wrapper'>
-      <div className='login-form'>
+      <form onSubmit={onSubmitLogin} className='login-form'>
         <h1>Login Form 🐞</h1>
-        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
+        {error && <div className='errorMessage'>{error?.message}</div>}
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input 
+            id={'email'}
+            type={'email'}
+            autoComplete='off'
+            value={email}
+            onChange={onChangeInput}
+          />
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input
+            id={'password'}
+            type={'password'}
+            value={password}
+            onChange={onChangeInput}
+          />
         </div>
 
         <div className='button'>
-          <button>Login</button>
+          <button type='submit' disabled={disabled}>Login</button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
